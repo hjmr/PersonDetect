45408f6179d4cff4b552fa50a41ad441c6641627
Hajime Murao <2620990+hjmr@users.noreply.github.com>
2018-10-17 20:09:23 +0900

    Modifed to detect and extract person from images.

--- Objectdetection.py
+++ Objectdetection.py
@@ -1,9 +1,7 @@
-n the rest of the tutorial, we assume that the `plt`
-# is imported before every code snippet.
-import matplotlib.pyplot as plt
+import os.path
 import argparse
+import matplotlib.pyplot as plt
 import cv2
-
 import numpy as np
 
 from chainercv.datasets import voc_bbox_label_names
@@ -13,9 +11,12 @@ from chainercv.visualizations import vis_bbox
 
 LABEL_ID_PERSON = 14
 
+
 def parse_arg():
     parser = argparse.ArgumentParser(description='Blah Blah')
     parser.add_argument('-t', '--threshold', type=float, default=0.6, help='set the score threshold.')
+    parser.add_argument('-o', '--out_dir', type=str,
+                        help='name of the directory where detected person images will be saved to.')
     parser.add_argument('images', type=str, nargs='+', help='a list of images.')
     return parser.parse_args()
 
@@ -31,28 +32,27 @@ def pickup_person(bbox_all, labels_all, scores_all):
             scores.append(score)
     return np.array(bboxes), np.array(labels), np.array(scores)
 
+
 arg = parse_arg()
 plt.ion()
-for img in arg.images:
-    img = read_image(img)
+for img_file in arg.images:
+    print("processing [{}] ...".format(img_file))
+    img = read_image(img_file)
     model = YOLOv3(pretrained_model='voc0712')
     model.score_thresh = arg.threshold
     bboxes, labels, scores = model.predict([img])
     bboxes, labels, scores = pickup_person(bboxes[0], labels[0], scores[0])
-    vis_bbox(img, bboxes, labels, scores, label_names=voc_bbox_label_names)
-    plt.draw()
-    key = input("please input [return] key.")
-    plt.close()
-
-
-for (x,y,w,h) in arg.images:
-    print("[x,y] = %d,%d [w,h] = %d,%d" %(x, y, w, h))
-    cv2.rectangle(img, (x,y), (x+w, y+h), (0, 0, 255), thickness=10)
-plt.show()
-cv2.imwrite("../output/out.jpg",img)
-
-for (x,y,w,h) in arg.images:
-    face_img = origin_img[y:y+h, x:x+w]
-    filename = "face_" + str(x) + "-" + str(y) + ".jpg"
-    cv2.imwrite(filename, face_img)
-
+    img = img.transpose(1, 2, 0).astype(np.uint8)  # convert image format from CHW to HWC
+    for idx, bbox in enumerate(bboxes):
+        bbox = bbox.astype(np.int)
+        person_img = img[bbox[0]:bbox[2], bbox[1]:bbox[3]]
+        if arg.out_dir is not None:
+            img_file = os.path.basename(img_file)
+            out_base, out_ext = os.path.splitext(img_file)
+            out_file = "{}/{}-{}{}".format(arg.out_dir, out_base, idx, out_ext)
+            cv2.imwrite(out_file, person_img)
+            print("ourput a person to {} ...".format(person_img))
+        else:
+            plt.imshow(face_img)
+            plt.show()
+            input("please input [return] key.")
